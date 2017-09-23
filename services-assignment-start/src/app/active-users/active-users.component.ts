68c1a9112faf84e20a34b6155509798e281abe4e
unknown <bhagathyennemajalu@gmail.com>
2017-09-23 20:20:03 +0530

    services

---
+++ services-assignment-start/src/app/active-users/active-users.component.ts
@@ -0,0 +1,17 @@
+import { UserService } from '../user-service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-active-users',
+  templateUrl: './active-users.component.html',
+  styleUrls: ['./active-users.component.css']
+})
+export class ActiveUsersComponent {
+  @Input() users: string[];
+  @Output() userSetToInactive = new EventEmitter<number>();
+
+  constructor(private userService:UserService){}
+  onSetToInactive(id: number) {
+    this.userService.setToActive(id);
+  }
+}
